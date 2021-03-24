73c56c265dae1e7a98045a97019321b0fa5e985c
unknown <randy.clound@gmail.com>
2021-03-24 20:16:52 +0800

    second commit

---
+++ app/src/main/java/com/randy/bfaa1/User.kt
@@ -0,0 +1,16 @@
+package com.randy.bfaa1
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    var avatar: Int? = 0,
+    var username: String? = "",
+    var name: String? = "",
+    var location: String? = "",
+    var company: String? = "",
+    var repository: String? = "",
+    var followers: String? = "",
+    var following: String? = ""
+) : Parcelable
